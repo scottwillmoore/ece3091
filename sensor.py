5de83da9ce8fad3085054d8f248d37898f39155c
Scott Moore <scott.will.moore@gmail.com>
2021-08-20 10:00:46 +1000

    Add example code to drive the ultrasonic distance sensor

---
+++ sensor.py
@@ -0,0 +1,13 @@
+# https://gpiozero.readthedocs.io/en/stable/api_input.html#distancesensor-hc-sr04
+
+from gpiozero import DistanceSensor
+from time import sleep
+
+FRONT_SENSOR_ECHO_PIN = 38
+FRONT_SENSOR_TRIGGER_PIN = 13
+
+frontSensor = DistanceSensor(FRONT_SENSOR_ECHO_PIN, FRONT_SENSOR_TRIGGER_PIN)
+
+while True:
+    print(f"distance: {frontSensor.distance}")
+    sleep(1)
