ece4ee4aaa8ac761bb981e4076e72d26eacc414a
Scott Moore <scott.will.moore@gmail.com>
2021-10-12 16:14:06 +1100

    Fix test_camera

--- src/gordon_launch/launch/test_camera.launch.py
+++ src/gordon_launch/launch/test_camera.launch.py
@@ -4,7 +4,11 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     camera = Node(
-        package="v4l2_camera", executable="v4l2_camera_node", namespace="camera", name="camera"
+        package="v4l2_camera",
+        executable="v4l2_camera_node",
+        namespace="camera",
+        name="camera",
+        parameters=[{"camera_frame_id": "camera_link"}],
     )
 
     return LaunchDescription([camera])
