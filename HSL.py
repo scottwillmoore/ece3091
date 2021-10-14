a98852ba621eeef7ec94c68a2e2b36a4fd293188
taylorjadechristie <88174702+taylorjadechristie@users.noreply.github.com>
2021-10-14 19:20:11 +1100

    Soz, this is actually SolvePnP with Cv2

--- HSL.py
+++ HSL.py
@@ -7,18 +7,17 @@ size = img.shape
 
 plt.imshow(img)
 # blue box, green box, red box, dfrobot box, camera box, weetbix box, chessbox
-points_2D = np.array([(336.9,390.6), (335,251), (335,176.5), (160.6,249.1), (516.1,251.9), (475.6,173.7), (336,125.6), (222.8,124.6)], dtype="double")
+points_2D = np.array([(338,344), (336,230), (336,144), (139,152), (573,21), (503,13), (218,83), (338,18),(336.9,390.6), (335,251), (335,176.5), (160.6,249.1), (516.1,251.9), (475.6,173.7),  (222.8,124.6),(336,125.6)], dtype="double")
 xVec = []
 yVec = []
-for i in range(0,8):
+for i in range(0,16):
     xVec.append(points_2D[i][0])
     yVec.append(points_2D[i][1])
-print(xVec)
-print(yVec)
-plt.plot(xVec,yVec,'*',markersize=2,color='r')
+
+plt.plot(xVec,yVec,'*',markersize=4,color='r')
 plt.show()
 
-points_3D = np.array([(30,0,25), (40,0,15), (50, 0, 25), (40,-10,61), (40,10,120),(50,10,107),(60,0,87),(60,-10,36)],dtype="double")
+points_3D = np.array([(30,0,25), (40,0,15), (50, 0, 25), (40,-10,61), (40,10,120),(50,10,107),(60,0,87),(60,-10,36),(30,0,0), (40,0,0), (50, 0, 0), (40,-10,0), (40,10,0),(50,10,0),(60,0,0),(60,-10,0)],dtype="double")
 
 dist_coeffs = np.zeros((4,1))
 
@@ -29,3 +28,5 @@ camera_matrix = np.array([[focal_length, 0, center[0]], [0, focal_length, center
 (success, rotation_vector,translation_vector) = cv2.solvePnP(points_3D,points_2D,camera_matrix,dist_coeffs,flags=0)
 print(rotation_vector)
 print(translation_vector)
+translation_vector_m = translation_vector/1000
+print(translation_vector_m)
