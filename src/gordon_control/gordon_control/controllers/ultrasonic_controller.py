be5bf1a4980a5bddadbe304c490e9ffb75cef1df
Taylor <tchr0002@student.monash.edu>
2021-10-19 16:13:52 +1100

    e

--- src/gordon_control/gordon_control/controllers/ultrasonic_controller.py
+++ src/gordon_control/gordon_control/controllers/ultrasonic_controller.py
@@ -5,12 +5,7 @@ class UltrasonicSensor:
     def range(self) -> float:
         return self._sensor.distance
 
-
     def __init__(self, echo_pin: int, trigger_pin: int):
         self._echo_pin = echo_pin
         self._trigger_pin = trigger_pin
         self._sensor = DistanceSensor(self._echo_pin,self._trigger_pin)
-
-sensor2=UltrasonicSensor(24,23)
-print(sensor2.range)
-
