84ab1b4b142e3ed3a1f9791162e378cd6346b1f5
Taylor <tchr0002@student.monash.edu>
2021-10-19 13:56:00 +1100

    help me learn to commit

--- src/gordon_control/gordon_control/controllers/ultrasonic_controller.py
+++ src/gordon_control/gordon_control/controllers/ultrasonic_controller.py
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-class UltrasonicController:
-=======
 from gpiozero import DistanceSensor
 
 class UltrasonicSensor:
->>>>>>> a043740b232e6382b359af95b1dbf047e977588b
     @property
     def range(self) -> float:
         return self._range
 
-<<<<<<< HEAD
-    _range: float = 0.0
-        
-    def __init__(self) -> None:
-
-    def update(self) -> None:
-        distance = x
-
-        self._range = distance
-=======
     def __init__(self, echo_pin: int, trigger_pin: int):
         self._echo_pin = echo_pin
         self._trigger_pin = trigger_pin
@@ -27,4 +13,3 @@ class UltrasonicSensor:
         sensor = DistanceSensor(self._echo_pin,self._trigger_pin)
         distance = sensor.distance
         self._range = distance
->>>>>>> a043740b232e6382b359af95b1dbf047e977588b
