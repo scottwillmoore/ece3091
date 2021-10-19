f7cb54aeed4c64d54b6d1b15cf0f90a9c44ce1a2
Taylor <tchr0002@student.monash.edu>
2021-10-19 15:39:48 +1100

    u

--- src/gordon_control/gordon_control/controllers/ultrasonic_controller.py
+++ src/gordon_control/gordon_control/controllers/ultrasonic_controller.py
@@ -1,15 +1,14 @@
 from gpiozero import DistanceSensor
 
 class UltrasonicSensor:
-    @property
-    def range(self) -> float:
-        return self._range
-
-    def __init__(self, echo_pin: int, trigger_pin: int):
+    def __init__(self, echo_pin: int, trigger_pin: int, range: int=0):
         self._echo_pin = echo_pin
         self._trigger_pin = trigger_pin
-
+        self._sensor = DistanceSensor(self._echo_pin,self._trigger_pin)
+    
     def read_distance(self):
-        sensor = DistanceSensor(self._echo_pin,self._trigger_pin)
-        distance = sensor.distance
-        self._range = distance
+        range = self._sensor.distance
+        print(range)
+        self.range = range
+
+
